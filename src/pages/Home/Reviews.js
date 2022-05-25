0cd70b18911bf9ff3db9ddbb420b9c9ee8fcdab3
Shama <assiddik001@gmail.com>
2022-05-26 02:06:58 +0600

    review

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/review").then((res) =>
+      res.json().then((data) => setReviews(data))
+    );
+  }, []);
+
+  const reverseReview = [...reviews].reverse();
+
   return (
     <div>
       <h2>Reviews</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {reverseReview.map((review) => (
+          <div key={review._id} class="rounded-lg shadow-xl bg-white p-10">
+            <img
+              src={review.img}
+              alt=""
+              class="rounded-full p-4 h-40 mx-auto"
+            />
+            <header class=" text-2xl font-extrabold py-4 px-4 text-center">
+              {review.name}
+            </header>
+            <div>
+              <ul class="text-gray-500 text-center font-semibold">
+                <li>Rating </li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
