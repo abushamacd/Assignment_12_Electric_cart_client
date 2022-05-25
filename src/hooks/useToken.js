3199bb6836988b896c6a8b8a613300c669baa052
Shama <assiddik001@gmail.com>
2022-05-25 14:11:40 +0600

    set token to local storage

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -16,6 +16,9 @@ const useToken = (user) => {
         .then((res) => res.json())
         .then((data) => {
           console.log("Inside token", data);
+          const accessToken = data.token;
+          localStorage.setItem("accessToken", accessToken);
+          setToken(accessToken);
         });
     }
   }, [user]);
